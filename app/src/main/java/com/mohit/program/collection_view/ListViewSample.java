5fa9dd8c192abf13ec0a93c6b998c57b912cf03f
Mohit Soni <mohit110891@gmail.com>
2018-07-11 13:33:12 +0530

    11-07

--- app/src/main/java/com/mohit/program/collection_view/ListViewSample.java
+++ app/src/main/java/com/mohit/program/collection_view/ListViewSample.java
@@ -1,7 +1,9 @@
 package com.mohit.program.collection_view;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -11,6 +13,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mohit.program.R;
+
+import java.util.ArrayList;
 
 /**
  * Author @ Mohit Soni on 26-04-2018 18:04.
@@ -71,5 +75,41 @@ public class ListViewSample extends Activity {
             }
             return view;
         }
+    }
+
+    public class Setter extends BaseAdapter {
+        ArrayList<String> modelArrayList = new ArrayList<>();
+        LayoutInflater layoutInflater;
+        Context context;
+
+        public Setter(ArrayList<String> modelArrayList, Context context) {
+            this.modelArrayList = modelArrayList;
+            this.context = context;
+
+            layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return modelArrayList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return modelArrayList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return modelArrayList.size();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            TextView textView = (TextView) convertView.findViewById(android.R.id.text1);
+            return convertView;
+        }
+
     }
 }
